8e8e49b47556df5405e9166ba4c814350876e214
Jongkeun Choi (Miel) <jongkeun.ch@gmail.com>
2022-05-28 13:43:46 +0900

    fix

--- src/rust-wasm/src/lib.rs
+++ src/rust-wasm/src/lib.rs
@@ -1,7 +1,5 @@
 extern crate rand;
 
-use std::thread;
-use std::time::Duration;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -11,12 +9,12 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+    a + b + 765
 }
 
 #[wasm_bindgen]
 pub fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
+    // thread::sleep(Duration::from_secs(2));
     intensity
 }
